1d2cddfd20c277ff0e9981be549a4fdd35e15599
Chakshu Pikalmunde <75752062+chakshu31@users.noreply.github.com>
2022-10-02 15:20:55 +0530

    Added Solution for problem 6

---
+++ Problems/Problem6/chakshu_pikalmunde.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main(){
+	int n;cin>>n;
+	if(n%4==0){
+	    cout<<n<<" is a leap year\n";
+	}
+	else{
+	    cout<<n<<" is not a leap year\n";
+	}
+}
